9dbff9c29ed8df6afacfe950f819b513e6aa9a56
lyoumbi <nkyoli@gmail.com>
2022-10-06 14:42:44 -0400

    Adding page website

---
+++ src/main/java/com/awscodebuild/lyoumbi/LyoumbiApplication.java
@@ -0,0 +1,13 @@
+package com.awscodebuild.lyoumbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyoumbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyoumbiApplication.class, args);
+	}
+
+}
